1150d83b07f37aeb7609881c8368aa4593d0d54a
lakeyk <lakeyk@google.com>
2023-11-15 21:36:56 -0800

    Add support for last name initial

--- src/video_coder/audio_utils.py
+++ src/video_coder/audio_utils.py
@@ -3,22 +3,9 @@ from threading import Thread, Lock
 import ffmpeg
 import moviepy.editor as mp
 import speech_recognition as sr
+from video_coder import names_helper as names
 
 SUPPORTED_EXTENSIONS = [".mp4", ".MP4", ".MOV", ".mov"]
-names_dict = {}
-
-# TODO: Move this to a config file
-def init_names_dict():
-	names_dict["Kelsi"] = ["Kelsi", "Kelsey"]
-	names_dict["Tatum"] = ["Tatum"]
-	names_dict["Adrian"] = ["Adrian", "Adrieene", "Adrien"]
-	names_dict["Ariana"] = ["Ariana", "Arieane", "Arriana"]
-	names_dict["Bella"] = ["Bella"]
-	names_dict["Boone"] = ["Boone", "Boon", "Boom", "boon", "boom"]
-	names_dict["Caman"] = ["Caman", "Canaan", "Cannon"]
-	names_dict["Carl"] = ["Carl", "Karl"]
-	names_dict["Carter"] = ["Carter", "Karter"]
-	names_dict["Charlie"] = ["Charlie"]
 
 def remove_audio(videos_path, video_file_name):
 	if video_file_name[-4:] in SUPPORTED_EXTENSIONS:
@@ -47,8 +34,7 @@ def remove_audio(videos_path, video_file_name):
 				exit()
 
 def autotag_file(videos_path, video_file_name):
-	if len(names_dict) == 0:
-		init_names_dict()
+	names_dict = names.get_names_dict()
 	r = sr.Recognizer()
 	detected = None
 	if video_file_name[-4:] not in SUPPORTED_EXTENSIONS:
@@ -70,7 +56,10 @@ def autotag_file(videos_path, video_file_name):
 			# recognize (convert from speech to text)
 			text = r.recognize_google(audio_data)
 			words = text.split(' ')
-			for word in words:
+			for i in range(len(words)):
+				word = words[i]
+				if i + 1 < len(words) and words[i+1] in names.INITIALS:
+					word = word + ' ' + words[i+1]
 				for name in names_dict:
 					if word in names_dict[name]:
 						# Rename file with name label if isn't already labeled
@@ -81,7 +70,8 @@ def autotag_file(videos_path, video_file_name):
 						os.rename(base_path, os.path.join(videos_path, new_file_name))
 						detected = name
 						break
-	except:
+	except Exception as e:
 		print('Issue running autotagger')
+		print(e)
 	os.remove(audio_file_name)
 	return detected
